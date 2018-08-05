0360d4b91b081632d25e0bab259e05aa1df2f77a
ArturT <arturtrzop@gmail.com>
2018-08-05 13:07:17 +0200

    Fix tslint tips

--- src/knapsack-pro-api.ts
+++ src/knapsack-pro-api.ts
@@ -3,41 +3,41 @@ import axios, { AxiosPromise } from "axios";
 import { TestFile } from "./test-file.model";
 
 // TODO: use fake env data for testing
-process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN = '0437763d32539d6162672d646704941b';
-process.env.KNAPSACK_PRO_FIXED_QUEUE_SPLIT = 'false';
-process.env.KNAPSACK_PRO_COMMIT_HASH = 'ae3396177d9f8ca87e2b93b4b0a25babd09d574d';
-process.env.KNAPSACK_PRO_BRANCH = 'master';
-process.env.KNAPSACK_PRO_NODE_TOTAL = '2';
-process.env.KNAPSACK_PRO_NODE_INDEX = '1';
-// process.env.KNAPSACK_PRO_NODE_BUILD_ID = '1234';
-process.env.KNAPSACK_PRO_NODE_BUILD_ID = new Date().getTime() + ''; // TODO: use this for testing
+process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN = "0437763d32539d6162672d646704941b";
+process.env.KNAPSACK_PRO_FIXED_QUEUE_SPLIT = "false";
+process.env.KNAPSACK_PRO_COMMIT_HASH = "ae3396177d9f8ca87e2b93b4b0a25babd09d574d";
+process.env.KNAPSACK_PRO_BRANCH = "master";
+process.env.KNAPSACK_PRO_NODE_TOTAL = "2";
+process.env.KNAPSACK_PRO_NODE_INDEX = "1";
+// process.env.KNAPSACK_PRO_NODE_BUILD_ID = "1234";
+process.env.KNAPSACK_PRO_NODE_BUILD_ID = new Date().getTime() + ""; // TODO: use this for testing
 
 export class KnapsackProAPI {
   private readonly apiBaseUrl: string;
 
   constructor() {
-    this.apiBaseUrl = 'https://api-staging.knapsackpro.com/v1';
+    this.apiBaseUrl = "https://api-staging.knapsackpro.com/v1";
   }
 
   // allTestFiles in whole user's test suite
-  fetchTestsFromQueue(allTestFiles: TestFile[], initializeQueue: boolean): AxiosPromise<any> {
+  public fetchTestsFromQueue(allTestFiles: TestFile[], initializeQueue: boolean): AxiosPromise<any> {
     const url = `${this.apiBaseUrl}/queues/queue`;
     const data = {
       test_suite_token: process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN,
       can_initialize_queue: initializeQueue,
-      fixed_queue_split: process.env.KNAPSACK_PRO_FIXED_QUEUE_SPLIT === 'true',
+      fixed_queue_split: process.env.KNAPSACK_PRO_FIXED_QUEUE_SPLIT === "true",
       commit_hash: process.env.KNAPSACK_PRO_COMMIT_HASH,
       branch: process.env.KNAPSACK_PRO_BRANCH,
       node_total: process.env.KNAPSACK_PRO_NODE_TOTAL,
       node_index: process.env.KNAPSACK_PRO_NODE_INDEX,
       node_build_id: process.env.KNAPSACK_PRO_NODE_BUILD_ID,
-      test_files: allTestFiles
+      test_files: allTestFiles,
     };
 
     return axios.post(url, data);
   }
 
-  createBuildSubset(recordedTestFiles: TestFile[]): AxiosPromise<any> {
+  public createBuildSubset(recordedTestFiles: TestFile[]): AxiosPromise<any> {
     const url = `${this.apiBaseUrl}/build_subsets`;
     const data = {
       test_suite_token: process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN,
@@ -45,7 +45,7 @@ export class KnapsackProAPI {
       branch: process.env.KNAPSACK_PRO_BRANCH,
       node_total: process.env.KNAPSACK_PRO_NODE_TOTAL,
       node_index: process.env.KNAPSACK_PRO_NODE_INDEX,
-      test_files: recordedTestFiles
+      test_files: recordedTestFiles,
     };
 
     return axios.post(url, data);
