690f3b6c524b82e7ff535ceca1c84445d3dc7c93
Rafał Trzop <rafaltrzop@gmail.com>
2018-12-23 18:08:59 +0100

    Add winston logger

--- src/knapsack-pro-logger.ts
+++ src/knapsack-pro-logger.ts
@@ -1,7 +1,33 @@
 import { AxiosError, AxiosResponse } from "axios";
 import util = require("util");
+import {
+  createLogger,
+  format,
+  Logger,
+  transports,
+} from "winston";
+
+// tslint:disable-next-line:no-var-requires
+const { name: clientName } = require("../../package.json");
 
 export class KnapsackProLogger {
+  private logger: Logger;
+
+  constructor() {
+    this.logger = createLogger({
+      level: "silly", // TODO: based on env || 'info'
+      format: format.combine(
+        format.label({ label: clientName }),
+        format.timestamp(),
+        format.colorize(),
+        format.printf(({ timestamp, label, level, message }) => `${timestamp} [${label}] ${level}: ${message}`),
+      ),
+      transports: [
+        new transports.Console(),
+      ],
+    });
+  }
+
   public logResponse(response: AxiosResponse<any>) {
     // tslint:disable-next-line:no-console
     console.log(util.inspect(response.data, {
@@ -13,9 +39,18 @@ export class KnapsackProLogger {
 
   public logError(error: AxiosError) {
     if (error.response) {
-      this.logResponse(error.response);
+      this.logger.error("error message");
+      this.logger.warn("warn message");
+      this.logger.info("info message");
+      this.logger.verbose("verbose message");
+      this.logger.debug("debug message");
+      this.logger.silly("silly message");
+
+      console.log("AAAAAAAAAAAA");
+      // this.logResponse(error.response);
     } else {
-      console.error(error);
+      console.log("BBBBBBBBBBBB");
+      // console.error(error);
     }
   }
 }
