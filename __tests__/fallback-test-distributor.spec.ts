402f6c4672cefea5df2e6454162fbe4d5ade4455
Artur Trzop <arturtrzop@gmail.com>
2018-12-27 18:32:31 +0100

    Add Jest
    * Use ts-jest https://github.com/kulshekhar/ts-jest
    * Add example spec

---
+++ __tests__/fallback-test-distributor.spec.ts
@@ -0,0 +1,10 @@
+import { FallbackTestDistributor } from '../src/fallback-test-distributor';
+
+describe('FallbackTestDistributor', () => {
+  // TODO
+  describe('example', () => {
+    it('adds 1 + 2 to equal 3', () => {
+      expect(1 + 2).toBe(3);
+    });
+  });
+});
