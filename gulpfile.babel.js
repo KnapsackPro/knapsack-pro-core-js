7946bcfedb4dd8ea29c15477a414fad72c69aa0a
Artur Trzop <arturtrzop@gmail.com>
2018-12-27 20:52:43 +0100

    Copy package.json to lib during compilation and watching in order to use path from lib directory when require
    This fixes problem with running tests in Jest and missing package.json
    file.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,35 +1,34 @@
-import gulp from 'gulp';
-import del from 'del';
-import ts from 'gulp-typescript';
+import gulp from "gulp";
+import del from "del";
+import ts from "gulp-typescript";
 
-const tsProject = ts.createProject('tsconfig.json');
+const filesToCopy = ["./package.json"];
+const tsProject = ts.createProject("tsconfig.json");
+
 const paths = {
-  src: tsProject.config.include,
-  dest: tsProject.config.compilerOptions.outDir,
+  src: [...tsProject.config.include, ...filesToCopy],
+  dest: tsProject.config.compilerOptions.outDir
 };
 
 export function clean() {
-  return del([
-    `${paths.dest}/**`,
-    `!${paths.dest}`
-  ]);
+  return del([`${paths.dest}/**`, `!${paths.dest}`]);
+}
+
+export function copy() {
+  return gulp.src(filesToCopy).pipe(gulp.dest(paths.dest));
 }
 
 export function compile() {
-  return tsProject.src()
+  return tsProject
+    .src()
     .pipe(tsProject())
-    .js
-    .pipe(gulp.dest(paths.dest));
+    .js.pipe(gulp.dest(paths.dest));
 }
 
 export function watch() {
-  gulp.watch(paths.src, compile);
+  gulp.watch(paths.src, gulp.parallel(copy, compile));
 }
 
-export const build = gulp.series(
-  clean,
-  compile,
-  watch
-);
+export const build = gulp.series(clean, copy, compile, watch);
 
 export default build;
