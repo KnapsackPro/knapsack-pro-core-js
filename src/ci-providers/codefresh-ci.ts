bbe858e122963fafd9c12800e1244344af2503ec
Artur Trzop <arturtrzop@gmail.com>
2019-10-11 18:38:07 +0200

    Add support for Codefresh.io CI provider

---
+++ src/ci-providers/codefresh-ci.ts
@@ -0,0 +1,24 @@
+import { CIProviderBase } from '.';
+
+// https://codefresh.io/docs/docs/codefresh-yaml/variables/#system-provided-variables
+export class CodefreshCI extends CIProviderBase {
+  public static get ciNodeTotal(): void {
+    return undefined;
+  }
+
+  public static get ciNodeIndex(): void {
+    return undefined;
+  }
+
+  public static get ciNodeBuildId(): string | void {
+    return process.env.CF_BUILD_ID;
+  }
+
+  public static get commitHash(): string | void {
+    return process.env.CF_REVISION;
+  }
+
+  public static get branch(): string | void {
+    return process.env.CF_BRANCH;
+  }
+}
