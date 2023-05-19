416592716953b4afe3b871c39579e4e7256145c8
3v0k4 <riccardo.odone@gmail.com>
2023-05-19 11:22:56 +0200

    feat: KNAPSACK_PRO_CI_NODE_BUILD_ID required

---
+++ __tests__/knapsack-pro-env.config.spec.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+import { KnapsackProEnvConfig } from '../src/config/knapsack-pro-env.config';
+
+describe('KnapsackProEnvConfig', () => {
+  // Since we use CircleCI for testing, we prevent it from interfering with the tests.
+  delete process.env.CIRCLE_BUILD_NUM;
+
+  const ENV = { ...process.env };
+
+  afterEach(() => {
+    process.env = ENV;
+  });
+
+  describe('.ciNodeBuildId', () => {
+    describe('when KNAPSACK_PRO_CI_NODE_BUILD_ID is not defined on the environment', () => {
+      it('throws', () => {
+        expect(() => KnapsackProEnvConfig.ciNodeBuildId).toThrow(
+          /Missing environment variable KNAPSACK_PRO_CI_NODE_BUILD_ID/,
+        );
+      });
+    });
+
+    describe('when the build id is defined on a supported CI environment', () => {
+      it('returns it', () => {
+        const ciBuildId = 'some-build-id';
+        process.env.GITHUB_RUN_ID = ciBuildId;
+
+        expect(KnapsackProEnvConfig.ciNodeBuildId).toEqual(ciBuildId);
+      });
+    });
+
+    describe('when KNAPSACK_PRO_CI_NODE_BUILD_ID is defined on the environment', () => {
+      it('returns it', () => {
+        const ciBuildId = 'some-build-id';
+        process.env.KNAPSACK_PRO_CI_NODE_BUILD_ID = ciBuildId;
+        process.env.GITHUB_RUN_ID = 'some-other-build-id';
+
+        expect(KnapsackProEnvConfig.ciNodeBuildId).toEqual(ciBuildId);
+      });
+    });
+  });
+});
